b220d2002db0ebf88ebe9a043f126f1bd6e52080
Vinki7 <xvinklers@stuba.sk>
2025-03-01 16:23:10 +0100

    feat(app-layout): app layout file created, minor changes to app settings

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,1 +1,7 @@
-I am here man! The home index!!!
+@extends('layouts.app')
+@section('title', 'Home - ReadMe')
+
+@section('content')
+    <h1>Welcome to ReadMe</h1>
+    <p>This is the home page of ReadMe store.</p>
+@endsection
