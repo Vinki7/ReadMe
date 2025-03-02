1c0c9cd943b3bd259881c70ee4413624ada9ce8d
Vinki7 <xvinklers@stuba.sk>
2025-03-02 11:49:09 +0100

    feat(styles): fonts imported and set up

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <img class="brand-logo" src="/assets/shs_logo.png" alt="logo" aria-hidden="true" [routerLink]="['/']" />
     <nav>
-        <ul class="nav-container bg-secondary">
+        <ul class="nav-container">
             <li class="nav-btn" [routerLink]="['/kanban']" routerLinkActive="active-link">
                 link 1
             </li>
@@ -13,5 +13,4 @@
     <div [ngClass]="{'hidden' : isDesktopView}" id="menu-btn-container" (click)="toggleSidebar()">
         <img [src]="moreButtonImagePath" alt="Menu">
     </div>
-    <p class="bg-secondary text-white p-3">This is a test</p>
 </header>
