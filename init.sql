7b0b968fe1d91491a7a90aaaac0ea57d3fb18639
Šimon Vinkler <simon.vinkler7@gmail.com>
2025-04-07 23:13:50 +0200

    feat: sql schema should be done

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
+-- Active: 1742515690970@@127.0.0.1@5678@readme@public
 -- ENUM types for various application-specific values
 CREATE TYPE role_value AS ENUM (
     'admin',
@@ -17,23 +18,29 @@ CREATE TYPE delivery_value AS ENUM (
     'same_day'
 );
 
-CREATE TYPE product_category_value AS ENUM (
-    'fantasy',
-    'sci-fi',
-    'mystery',
-    'romance',
-    'horror',
-    'non-fiction',
-    'biography',
-    'self_help',
-    'education',
-    'fitness',
-    'psychology',
-    'adults'
+CREATE TABLE categories (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    category_name VARCHAR(50) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+INSERT INTO categories (category_name, description) VALUES
+    ('fantasy', 'Fantasy books'),
+    ('sci-fi', 'Science Fiction books'),
+    ('mystery', 'Mystery books'),
+    ('romance', 'Romance books'),
+    ('horror', 'Horror books'),
+    ('non-fiction', 'Non-Fiction books'),
+    ('biography', 'Biography books'),
+    ('self_help', 'Self-Help books'),
+    ('education', 'Educational books'),
+    ('fitness', 'Fitness books'),
+    ('psychology', 'Psychology books'),
+    ('adults', 'Adult books');
 
 -- Users Table
-CREATE TABLE Users (
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(25) NOT NULL,
     surname VARCHAR(25) NOT NULL,
@@ -41,8 +48,8 @@ CREATE TABLE Users (
 );
 
 -- Credentials Table
-CREATE TABLE Credentials (
-    id UUID PRIMARY KEY REFERENCES Users(id) ON DELETE CASCADE,
+CREATE TABLE credentials (
+    id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     username VARCHAR(25) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL, -- 60 for bcrypt hash
     email VARCHAR(320) UNIQUE NOT NULL,
@@ -50,18 +57,18 @@ CREATE TABLE Credentials (
 );
 
 -- Carts Table
-CREATE TABLE Carts (
+CREATE TABLE carts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID REFERENCES Users(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     total_amount NUMERIC(10, 2) DEFAULT 0.00
 );
 
 -- Sessions Table
-CREATE TABLE Sessions (
+CREATE TABLE sessions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID REFERENCES Users(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     session_token VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP NOT NULL,
@@ -69,37 +76,44 @@ CREATE TABLE Sessions (
 );
 
 -- Products Table
-CREATE TABLE Products (
+CREATE TABLE products (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     price NUMERIC(10, 2) NOT NULL,
     stock INT NOT NULL DEFAULT 0,
-    category product_category_value NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Product Images Table
-CREATE TABLE ProductImages (
+CREATE TABLE product_categories (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    product_id UUID REFERENCES Products(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    category_id BIGINT REFERENCES categories(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+-- Product Images Table
+CREATE TABLE product_images (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
     image_data BYTEA NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 -- Cart Products Table
-CREATE TABLE CartProducts (
-    cart_id UUID REFERENCES Carts(id) ON DELETE CASCADE,
-    product_id UUID REFERENCES Products(id) ON DELETE CASCADE,
+CREATE TABLE cart_products (
+    cart_id UUID REFERENCES carts(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
     quantity INT NOT NULL DEFAULT 1,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 -- Authors Table
-CREATE TABLE Authors (
+CREATE TABLE authors (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
@@ -108,9 +122,34 @@ CREATE TABLE Authors (
 );
 
 -- Product Authors Table
-CREATE TABLE ProductAuthors {
-    product_id UUID REFERENCES Products(id) ON DELETE CASCADE,
-    author_id UUID REFERENCES Authors(id) ON DELETE CASCADE,
+CREATE TABLE product_authors (
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    author_id UUID REFERENCES authors(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-}
+);
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    delivery_method delivery_value NOT NULL,
+    delivery_date TIMESTAMP,
+    expedition_date TIMESTAMP,
+    delivery_address TEXT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    price NUMERIC(10, 2) NOT NULL
+);
+
+CREATE TABLE payments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+    date_of_payment TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method payment_value NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL
+);
+
+CREATE TABLE user_orders (
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
