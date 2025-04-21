a27dbc4ef31da9c3a2050a4b5f6c1fca40aa416a
Tomáš Zenka <tomas.zenka@pobox.sk>
2025-04-22 00:15:32 +0200

    fix: layout of the page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,157 +6,199 @@
 
 @section('content')
 <main class="container-fluid row row-cols-auto mx-0 justify-content-center">
-    <!-- Sidebar Filter (left) -->
-    <aside class="col-lg-3 d-none d-lg-block pt-5 ps-5">
-        <div class="filter-box">
-            <h5>Filter</h5>
-            <div class="price-card">
-                <label>Price Range</label>
-                <div class="price-content">
-                    <div>
-                        <label>Min</label>
-                        <p id="min-value">€0</p>
-                    </div>
-                    <div>
-                        <label>Max</label>
-                        <p id="max-value">€9999</p>
-                    </div>
-                </div>
+            <!-- Side filter panel -->
+            <aside class="col-lg-3 d-none d-lg-block pt-5 ps-5">
+                <div class="filter-box">
+                    <h5>Filter</h5>
+                    <div class="price-card">
+                        <label>
+                            Price Range
+                        </label>
+                        <div class="price-content">
+                            <div>
+                                <label>Min</label>
+                                <p id="min-value">€0</p>
+                            </div>
+                            <div>
+                                <label>Max</label>
+                                <p id="max-value">€9999</p>
+                            </div>
+                        </div>
 
-                <div class="range-slider">
-                    <div class="range-fill"></div>
-                    <input type="range" class="min-price" value="0" min="0" max="9990" step="10" />
-                    <input type="range" class="max-price" value="9990" min="0" max="9990" step="10" />
+                        <div class="range-slider">
+                            <div class="range-fill"></div>
+
+                            <input
+                                type="range"
+                                class="min-price"
+                                value="0"
+                                min="0"
+                                max="9990"
+                                step="10"
+                            />
+
+                            <input
+                                type="range"
+                                class="max-price"
+                                value="9990"
+                                min="0"
+                                max="9990"
+                                step="10"
+                            />
+                        </div>
+                    </div>
+
+
+
+                    <label class="form-label mt-3">Genre</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
+
+                    <label class="form-label mt-3">Author</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
+
+                    <label class="form-label mt-3">Language</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
+                </div>
+            </aside>
+
+            <!-- Small screen filter button -->
+            <div class="d-lg-none col-8 col-sm-3 text-center">
+                <button class="filter-button btn btn-charcoal d-lg-none col-12 my-3" data-bs-toggle="offcanvas" data-bs-target="#filterOffcanvas">
+                    Filter
+                </button>
+            </div>
+
+            <!-- Small screenfilter panel -->
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="filterOffcanvas">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title">Filter</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="filter-box">
+                        <label>Price Range</label>
+                        <div class="price-content">
+                            <div>
+                                <label>Min</label>
+                                <p id="min-value">€0</p>
+                            </div>
+                            <div>
+                                <label>Max</label>
+                                <p id="max-value">€9999</p>
+                            </div>
+                        </div>
+
+                        <div class="range-slider">
+                            <div class="range-fill"></div>
+                            <input
+                                type="range"
+                                class="min-price"
+                                value="0"
+                                min="0"
+                                max="9990"
+                                step="10"
+                            />
+
+                            <input
+                                type="range"
+                                class="max-price"
+                                value="9990"
+                                min="0"
+                                max="9990"
+                                step="10"
+                            />
+                        </div>
+                    </div>
+
+                    <label class="form-label mt-3">Genre</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
+
+                    <label class="form-label mt-3">Author</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
+
+                    <label class="form-label mt-3">Language</label>
+                    <select class="form-select">
+                        <option class="option-value">All</option>
+                        <option class="option-value">Option data1</option>
+                        <option class="option-value">Option data2</option>
+                        <option class="option-value">Option data3</option>
+                    </select>
                 </div>
             </div>
 
-            <label class="form-label mt-3">Genre</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
 
-            <label class="form-label mt-3">Author</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
-
-            <label class="form-label mt-3">Language</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
-        </div>
-    </aside>
-
-    <!-- Filter button (mobile) -->
-    <div class="d-lg-none col-8 col-sm-3 text-center">
-        <button class="filter-button btn btn-charcoal d-lg-none col-12 my-3" data-bs-toggle="offcanvas" data-bs-target="#filterOffcanvas">
-            Filter
-        </button>
-    </div>
-
-    <!-- Offcanvas (mobile filter) -->
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="filterOffcanvas">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title">Filter</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-        </div>
-        <div class="offcanvas-body">
-            <div class="filter-box">
-                <label>Price Range</label>
-                <div class="price-content">
-                    <div>
-                        <label>Min</label>
-                        <p id="min-value">€0</p>
+            <!-- Main area-->
+            <section class="col-12 col-lg-9 pt-5">
+                <!-- Search/order area -->
+                <div class="btn-container row row-cols-1 row-cols-md-2 align-items-center justify-content-center gap-3 gap-lg-0 gap mb-4 px-md-5">
+                    <!-- Search bar -->
+                    <div class="col">
+                        <input type="text" class="form-control col-12" placeholder="Search">
                     </div>
-                    <div>
-                        <label>Max</label>
-                        <p id="max-value">€9999</p>
+
+                    <!-- Ordering buttons -->
+                    <div class="col-7 col-sm-4 d-flex flex-column justify-content-end flex-md-row flex-wrap gap-2" role="group">
+                        <!-- Name sort buttons -->
+                        <div class="d-flex flex-column flex-xl-row gap-2">
+                            <div>
+                                <input type="radio" id="name-asc" name="sort" class="order-radio">
+                                <label for="name-asc" class="order-button btn btn-secondary">
+                                    Name <img src="../../public/images/icons/chevron_up_icon.png" class="order-icon">
+                                </label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="name-desc" name="sort" class="order-radio">
+                                <label for="name-desc" class="order-button btn btn-secondary">
+                                    Name <img src="../../public/images/icons/chevron_down_icon.png" class="order-icon">
+                                </label>
+                            </div>
+                        </div>
+                        <!-- Price Sort Buttons -->
+                        <div class="d-flex flex-column flex-xl-row gap-2">
+                            <div>
+                                <input type="radio" id="price-asc" name="sort" class="order-radio">
+                                <label for="price-asc" class="order-button btn btn-secondary">
+                                    Price <img src="../../public/images/icons/chevron_up_icon.png" class="order-icon">
+                                </label>
+                            </div>
+
+                            <div>
+                                <input type="radio" id="price-desc" name="sort" class="order-radio">
+                                <label for="price-desc" class="order-button btn btn-secondary">
+                                    Price <img src="../../public/images/icons/chevron_down_icon.png" class="order-icon">
+                                </label>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="range-slider">
-                    <div class="range-fill"></div>
-                    <input type="range" class="min-price" value="0" min="0" max="9990" step="10" />
-                    <input type="range" class="max-price" value="9990" min="0" max="9990" step="10" />
-                </div>
-            </div>
-
-            <label class="form-label mt-3">Genre</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
-
-            <label class="form-label mt-3">Author</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
-
-            <label class="form-label mt-3">Language</label>
-            <select class="form-select">
-                <option>All</option>
-                <option>Option data1</option>
-                <option>Option data2</option>
-                <option>Option data3</option>
-            </select>
-        </div>
-    </div>
-
-    <!-- Product Grid Section -->
-    <section class="col-12 col-lg-9 pt-5">
-        <!-- Search and Sorting -->
-        <div class="btn-container row row-cols-1 row-cols-md-2 align-items-center justify-content-center gap-3 mb-4 px-md-5">
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Search">
-            </div>
-
-            <div class="col-7 col-sm-4 d-flex flex-column flex-md-row flex-wrap gap-2">
-                <div class="d-flex flex-column flex-xl-row gap-2">
-                    <div>
-                        <input type="radio" id="name-asc" name="sort" class="order-radio">
-                        <label for="name-asc" class="order-button btn btn-secondary">
-                            Name <img src="/images/icons/chevron_up_icon.png" class="order-icon">
-                        </label>
-                    </div>
-                    <div>
-                        <input type="radio" id="name-desc" name="sort" class="order-radio">
-                        <label for="name-desc" class="order-button btn btn-secondary">
-                            Name <img src="/images/icons/chevron_down_icon.png" class="order-icon">
-                        </label>
-                    </div>
-                </div>
-                <div class="d-flex flex-column flex-xl-row gap-2">
-                    <div>
-                        <input type="radio" id="price-asc" name="sort" class="order-radio">
-                        <label for="price-asc" class="order-button btn btn-secondary">
-                            Price <img src="/images/icons/chevron_up_icon.png" class="order-icon">
-                        </label>
-                    </div>
-                    <div>
-                        <input type="radio" id="price-desc" name="sort" class="order-radio">
-                        <label for="price-desc" class="order-button btn btn-secondary">
-                            Price <img src="/images/icons/chevron_down_icon.png" class="order-icon">
-                        </label>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Dynamic Product Cards -->
+        <!-- Product Cards -->
         <div class="row row-cols-auto justify-content-center gap-4 pb-3 g-0">
             @forelse ($products as $product)
                 <x-product-listing-card
