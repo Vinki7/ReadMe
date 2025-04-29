fdb59b24825f6aa695d1dea915335d490c5b6bc5
Vinki7 <xvinklers@stuba.sk>
2025-04-29 20:41:28 +0200

    chore: removed stock field

--- app/DTOs/Product/ProductListingDto.php
+++ app/DTOs/Product/ProductListingDto.php
@@ -11,7 +11,6 @@ class ProductListingDto
     public string $title;
     public string $description;
     public float $price;
-    public int $stock;
     public string $category;
     public array $authors;
     public string $frontCover;
@@ -21,7 +20,6 @@ class ProductListingDto
         $this->title = $product->title;
         $this->description = $product->description;
         $this->price = $product->price;
-        $this->stock = $product->stock;
         $this->category = $product->category->value;
         $this->authors = $product->authors->map(function ($author) {
             return "{$author->name} {$author->surname}";
