2ff994231c18f1ff300f2c00bcfacae07778d82e
Vinki7 <xvinklers@stuba.sk>
2025-05-01 17:51:18 +0200

    chore: implemented validation error alert

--- resources/views/cart/address.blade.php
+++ resources/views/cart/address.blade.php
@@ -21,26 +21,44 @@
                 <section class="d-flex flex-column gap-2">
                     <label class="label">Full Name</label>
                     <input class="input" type="text" name="fullName" id="fullName" placeholder="Enter name" required>
+                    @error('fullName')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <label class="label">E-mail</label>
                     <input class="input" type="text" name="eMial" id="eMial" placeholder="Enter E-mail" required>
+                    @error('eMial')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <label class="label">Street Address</label>
                     <input class="input" type="text" name="streetAddress" id="streetAddress" placeholder="Enter address" required>
+                    @error('streetAddress')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <label class="label">City</label>
                     <input class="input" type="text" name="city" id="city" placeholder="Enter city" required>
+                    @error('city')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <label class="label">Postal Code</label>
                     <input class="input" type="text" name="postalCode" id="postalCode" placeholder="Enter postal code" required>
+                    @error('postalCode')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <label class="label">Country</label>
                     <input class="input" type="text" name="country" id="country" placeholder="Enter country" required>
+                    @error('country')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </section>
                 <section class="d-flex flex-column gap-2">
                     <h4 class="title">Choose delivery method:</h4>
