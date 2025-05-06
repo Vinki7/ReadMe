27e47c5c32136534cd3de7fd201844c8fcd53c6d
Vinki7 <xvinklers@stuba.sk>
2025-05-06 10:59:20 +0200

    chore: middleware staged

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,6 +16,10 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!Auth::check() || !Auth::user()->is_admin) {
+            abort(403, 'Unauthorized access.');
+        }
+
         return $next($request);
     }
 }
