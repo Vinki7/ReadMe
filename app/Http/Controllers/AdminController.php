3ad3088f8adc34eb86ff651a96ff4f43ff7797c2
Tomáš Zenka <tomas.zenka@pobox.sk>
2025-05-07 21:35:05 +0200

    feat: setup controller to be able to show all products

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\ProductService;
 
 class AdminController extends Controller
 {
+    protected ProductService $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+    
     public function index()
     {
-        return view('admin.admin-listing');
+        $products = $this->productService->getAllProducts();
+        return view('admin.admin-listing', compact('products'));
     }
 }
